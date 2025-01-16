70be9603680322fffec1bdc7b74b571e35ded4c7
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-16 13:13:26 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { AppDataSource } from './data-source';
+
+AppDataSource.initialize().then(() => {
+   const app = express();
+
+   app.use(express.json());
+
+   
+
+   return app.listen(process.env.PORT);
+});

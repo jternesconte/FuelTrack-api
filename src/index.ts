7f56e3c1b45c64d434ae16200e159a9303baa8de
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-16 13:30:42 -0300

    feat: implements car routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import { AppDataSource } from './data-source';
+import { carRoutes } from './routes/CarRoutes';
 
 AppDataSource.initialize().then(() => {
    const app = express();
 
    app.use(express.json());
 
-   
+   app.use('/api/car', carRoutes);
 
    return app.listen(process.env.PORT);
 });

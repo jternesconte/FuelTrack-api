6cbe9c176d38f92e4bfc50ecdebbc56217994b20
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-16 23:00:03 -0300

    Feat: creates Fuel entity

---
+++ src/entities/Fuel.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Car } from "./Car";
+import { format, isValid, parse } from "date-fns";
+
+@Entity('fuel')
+export class Fuel {
+   @PrimaryGeneratedColumn()
+   id: number;
+
+   @ManyToOne(() => Car, { nullable: false })
+   @JoinColumn({ name: "car_id", referencedColumnName: 'id' })
+   car: Car;
+   
+   @Column({ type: "decimal", precision: 10, scale: 2, nullable: false })
+   price: number;
+
+   @Column({ type: "decimal", precision: 10, scale: 2, nullable: false })
+   liters: number;
+
+   @Column({ name: 'distance_traveled', type: "decimal", precision: 10, scale: 1, nullable: true })
+   distanceTraveled: number;
+
+   @Column({ name: 'average_last_route', type: "decimal", precision: 10, scale: 2, nullable: true })
+   averageLastRoute: number;
+
+   @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", nullable: false })
+   date: Date;
+}

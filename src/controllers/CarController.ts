3db09d5ccee2032a44c30d52ab61471bce3ed93a
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-16 22:59:23 -0300

    feat: adds some variables to the method

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -1,11 +1,26 @@
 import { Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
+import { carRepository } from '../repositories/CarRepository';
 
 export class CarController {
 
    async newCar(req: Request, res: Response) {
       try {
-         const { model, engine, year, category } = req.body
+         const { model, engine, year, category, km, fuelCapacity, remainingFuel} = req.body;
          
+         const car: ICar = {
+            model,
+            engine,
+            year,
+            category,
+            km,
+            fuelCapacity,
+            remainingFuel
+         }
+
+         await carRepository.saveCar(car);
+
+         res.status(200).json(car);
       } catch {
          res.status(500).json({ error: 'Error in car creation' })
       }

e14c7fb3afc7e109ebf02abce0bc5808e2586b30
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-16 23:01:39 -0300

    feat: implements the saveFuel method

---
+++ src/repositories/FuelRepository.ts
@@ -0,0 +1,26 @@
+import { AppDataSource } from "../data-source";
+import { Fuel } from "../entities/Fuel";
+import { IFuel } from "../interfaces/IFuel";
+
+export const fuelRepository = AppDataSource.getRepository(Fuel).extend({
+
+ async saveFuel(data: IFuel): Promise<Fuel> {
+   const fuel = this.create({
+      car: data.car,
+      price: data.price,
+      liters: data.liters,
+      distanceTraveled: data.distanceTraveled,
+      date: data.date
+   });
+
+   return this.save(fuel);
+ },
+
+ async findLastByCarId(carId: number) {
+   return this.createQueryBuilder('fuel')
+     .where('fuel.car_id = :carId', { carId })
+     .orderBy('fuel.date', 'DESC')
+     .getOne();
+ }
+ 
+});

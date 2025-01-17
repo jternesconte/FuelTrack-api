7c731222e7ac1e843fd522c6e6056e3b5510b8b9
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-16 23:01:19 -0300

    feat: changes the saveCar method

--- src/repositories/CarRepository.ts
+++ src/repositories/CarRepository.ts
@@ -2,13 +2,16 @@ import { AppDataSource } from "../data-source";
 import { Car } from "../entities/Car";
 import { ICar } from "../interfaces/ICar";
 
-export const CarRepository = AppDataSource.getRepository(Car).extend({
+export const carRepository = AppDataSource.getRepository(Car).extend({
  async saveCar(data: ICar): Promise<Car> {
    const car = this.create({
       model: data.model,
       engine: data.engine,
       year: data.year,
-      category: data.category
+      km: data.km,
+      category: data.category,
+      fuelCapacity: data.fuelCapacity,
+      remainingFuel: data.remainingFuel
    });
 
    return this.save(car);

8ff73357c7a19e327459b0a920918851e1570b35
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-16 23:00:50 -0300

    feat: creates the fuel Interface

---
+++ src/interfaces/IFuel.ts
@@ -0,0 +1,11 @@
+import { Car } from "../entities/Car";
+import { ICar } from "./ICar";
+
+export interface IFuel {
+   car: ICar;
+   price: number;
+   liters: number;
+   distanceTraveled: number;
+   averageLastRoute: number
+   date: string;
+}

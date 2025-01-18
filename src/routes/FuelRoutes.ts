2f872de80b83e9ad903a7c5ec2ec315a50512f05
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-18 17:23:48 -0300

    refactor: changes route name

--- src/routes/FuelRoutes.ts
+++ src/routes/FuelRoutes.ts
@@ -3,4 +3,4 @@ import { FuelController } from "../controllers/FuelController";
 
 export const fuelRoutes = Router();
 
-fuelRoutes.post('/create/:carId', new FuelController().newFuel);
+fuelRoutes.post('/fuel/:carId', new FuelController().newFuel);

d8af45bbf913979cadea3829fd455020a4284864
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-20 07:31:40 -0300

    feat: adds import and changes title of app

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from './shared/components/app-header/app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Front';
+  title = 'FuelTrack';
 }

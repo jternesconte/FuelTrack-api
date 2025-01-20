3595caf02104f05d8c56b99557aaa24a56c464db
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-20 13:28:51 -0300

    build: changes file limit

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -6,6 +6,9 @@ import { fuelRoutes } from './routes/FuelRoutes';
 AppDataSource.initialize().then(() => {
    const app = express();
 
+   app.use(express.json({ limit: '50mb' }));
+   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
    app.use(express.json());
 
    app.use('/api/car', carRoutes);

82486685b421e9b1dbfd815fdf8c0caa22d5234e
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-21 07:43:11 -0300

    feat: creates averageCurrentMonth route

--- Back/src/routes/FuelRoutes.ts
+++ Back/src/routes/FuelRoutes.ts
@@ -4,3 +4,5 @@ import { FuelController } from "../controllers/FuelController";
 export const fuelRoutes = Router();
 
 fuelRoutes.post('/create/:carId', new FuelController().newFuel);
+
+fuelRoutes.get('/average/:carId', new FuelController().averageCurrentMonth);

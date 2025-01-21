723e64e9f4bfa11ad711d2958cc4f90f7c1bebaa
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-21 13:21:24 -0300

    feat: implements data of car information to show in carousel

--- Front/src/app/features/car-profile/car-profile.component.ts
+++ Front/src/app/features/car-profile/car-profile.component.ts
@@ -5,33 +5,53 @@ import { CarService } from '../../shared/services/car.service';
 import { CarDto } from '../../shared/interfaces/CarDto';
 import { Observable } from 'rxjs';
 import { FuelService } from '../../shared/services/fuel.service';
+import { CommonModule } from '@angular/common';
 
-export interface lastMonthData {
+export interface LastMonthData {
   averageConsumption: number;
   totalDistance: number;
   totalLiters: number;
+}
+
+export interface CarouselData {
+  title: string;
+  value: number;
 }
 
 @Component({
   selector: 'app-car-profile',
   templateUrl: './car-profile.component.html',
   styleUrls: ['./car-profile.component.css'],
-  imports: [CardModule, CarouselModule]
+  imports: [CardModule, CarouselModule, CommonModule]
 })
 export class CarProfileComponent implements OnInit {
 
+  carObservable!: Observable<CarDto>;
   car!: CarDto;
-  lastMonthInfo!: lastMonthData;
+  lastMonthInfo!: LastMonthData;
+  carouselData: CarouselData[] = [];
 
   constructor(
     private carService: CarService,
     private fuelService: FuelService) {
-    this.carService.getCarById(8).subscribe(r => this.car = r);
-    this.fuelService.getCarData(8).subscribe(r => this.lastMonthInfo  =r)
+    this.carObservable =  this.carService.getCarById(8);
+    this.fuelService.getCarData(8).subscribe(r => {
+      this.lastMonthInfo = r;
+      this.carouselData.push({title: 'Consumption Average', value: r.averageConsumption});
+      this.carouselData.push({title: 'Distance Traveled', value: r.totalDistance});
+      this.carouselData.push({title: 'Liters used', value: r.totalLiters});
+    })
   }
 
   ngOnInit() {
-
+    this.carObservable.subscribe({
+      next: (value) => {
+          this.car = value;
+      },
+      error: (error) => {
+        error
+      }
+    })
   }
 
   getCarInfo() {

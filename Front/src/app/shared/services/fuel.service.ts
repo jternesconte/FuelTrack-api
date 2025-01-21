ce6cc09d1e2719ad7c3fe5e446f819bf7b299941
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-21 13:20:03 -0300

    refactor: changes the variable letter case

--- Front/src/app/shared/services/fuel.service.ts
+++ Front/src/app/shared/services/fuel.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { lastMonthData } from '../../features/car-profile/car-profile.component';
+import { LastMonthData } from '../../features/car-profile/car-profile.component';
+import { FuelDto } from '../interfaces/FuelDto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,11 @@ export class FuelService {
 
   constructor(private http: HttpClient) { }
 
+  createFuel(fuelInfo: FuelDto, carId: number) {
+    return this.http.post(`http://localhost:3000/api/fuel/create/${carId}`, fuelInfo);
+  }
 
-  getCarData(carId: number): Observable<lastMonthData> {
-    return this.http.get<lastMonthData>(`http://localhost:3000/api/fuel/average/${carId}`);
+  getCarData(carId: number): Observable<LastMonthData> {
+    return this.http.get<LastMonthData>(`http://localhost:3000/api/fuel/average/${carId}`);
   }
 }

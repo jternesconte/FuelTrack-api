b944d55aeb996e032a80b8c99e6f7ce1340251a7
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-21 07:46:18 -0300

    feat: adds carDto interface

---
+++ Front/src/app/shared/interfaces/CarDto.ts
@@ -0,0 +1,11 @@
+
+export interface CarDto {
+   id?: number
+   model: string;
+   engine: string;
+   year: string;
+   category: string;
+   km: number
+   fuelCapacity: number;
+   image?: string;
+}

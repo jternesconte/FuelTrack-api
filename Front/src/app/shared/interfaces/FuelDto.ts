2a1c39ec0a7d4b93452c37a6271abcd6542a4c59
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-21 13:20:56 -0300

    feat: adds fuelDto

---
+++ Front/src/app/shared/interfaces/FuelDto.ts
@@ -0,0 +1,9 @@
+import { CarDto } from "./CarDto";
+
+export interface FuelDto {
+   price: number;
+   liters: number;
+   distanceTraveled: number;
+   averageLastRoute: number
+   date: string;
+}

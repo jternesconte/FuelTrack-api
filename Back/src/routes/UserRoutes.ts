8d9c5d9755faaba51993dd1c41761681c79a4f2b
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-22 13:10:37 -0300

    feat: implement user routes

---
+++ Back/src/routes/UserRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { UserController } from "../controllers/UserController";
+import { authenticateToken } from "../middlewares/authenticateToken";
+
+export const userRoutes = Router();
+
+userRoutes.post('/register', new UserController().newCadastro);
+
+userRoutes.post('/login', new UserController().userLogin);
+
+userRoutes.put('/editUser', authenticateToken, new UserController().editUser);

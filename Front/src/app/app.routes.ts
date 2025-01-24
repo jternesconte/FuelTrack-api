f5149e3f1dcf09c249b50a75fde498a53def48e7
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-24 07:56:58 -0300

    feat: aconfigures new routes

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { CarProfileComponent } from './features/car-profile/car-profile.component';
+import { LoginPageComponent } from './features/login-page/login-page.component';
+import { UserCarsComponent } from './features/user-cars/user-cars.component';
 
 export const routes: Routes = [
    {
+      path: 'login',
+      component: LoginPageComponent
+   },
+   {
       path: '',
+      component: UserCarsComponent
+   },
+   {
+      path: 'car/:carId',
       component: CarProfileComponent
    }
 ];

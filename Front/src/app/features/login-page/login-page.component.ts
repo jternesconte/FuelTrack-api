4da2689f20ab7e2d74c79918c2d06c28a68a4091
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-27 13:21:00 -0300

    feat: transforms login page in login/register component

--- Front/src/app/features/login-page/login-page.component.ts
+++ Front/src/app/features/login-page/login-page.component.ts
@@ -4,23 +4,32 @@ import { CardModule } from 'primeng/card';
 import { CarouselModule } from 'primeng/carousel';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { UserService } from '../../shared/services/user.service';
+import { TabsModule } from 'primeng/tabs';
 
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css'],
-  imports: [CardModule, CarouselModule, CommonModule, InputGroupModule, InputGroupAddonModule, InputTextModule]
+  styleUrls: ['./login-page.component.css', '../../../styles.css'],
+  imports: [CardModule, CarouselModule, CommonModule, InputGroupModule, InputGroupAddonModule, InputTextModule, ButtonModule, TabsModule]
 })
 export class LoginPageComponent implements OnInit {
 
   formGroup: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  loginData: any[] = [];
+  registerData: any[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService
+  ) {
     this.formGroup = this.fb.group({
-      name: '',
+      name:'',
       email: '',
       password: '',
       password2: ''
@@ -28,6 +37,20 @@ export class LoginPageComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onLoginSubmit() {
+    this.loginData.push(this.formGroup.get('email')?.value);
+    this.loginData.push(this.formGroup.get('password')?.value);
+    this.userService.userLogin(this.loginData)
+  }
+
+  onRegisterSubmit() {
+    this.registerData.push(this.formGroup.get('name')?.value);
+    this.registerData.push(this.formGroup.get('email')?.value);
+    this.registerData.push(this.formGroup.get('password')?.value);
+    this.registerData.push(this.formGroup.get('password2')?.value);
+    this.userService.userLogin(this.registerData)
   }
 
 }

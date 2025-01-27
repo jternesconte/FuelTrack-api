54c096cbfadb640f09d90cc67256984e6ce6edb9
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-27 13:21:50 -0300

    build: removes dark/light mode of primeng

--- Front/src/app/app.config.ts
+++ Front/src/app/app.config.ts
@@ -14,10 +14,13 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Aura
-        }
+            preset: Aura,
+            options: {
+              darkModeSelector: false || 'none'
+            }
+        },
     }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
   ]
 };

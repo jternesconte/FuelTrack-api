d1da275cd229cbd1a0f9e91fabaa048cd7a7b128
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-27 13:22:50 -0300

    style: adds styles.css to user-cars file

--- Front/src/app/features/user-cars/user-cars.component.ts
+++ Front/src/app/features/user-cars/user-cars.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-cars',
   templateUrl: './user-cars.component.html',
-  styleUrls: ['./user-cars.component.css']
+  styleUrls: ['./user-cars.component.css', '../../../styles.css']
 })
 export class UserCarsComponent implements OnInit {
 

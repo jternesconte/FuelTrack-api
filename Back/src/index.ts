a827473359ccd39becea7971fae7332fa7197216
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-29 13:24:13 -0300

    build: changes cors permissions

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -15,6 +15,7 @@ AppDataSource.initialize().then(() => {
    app.use((req, res, next) => {
        res.header("Access-Control-Allow-Origin", "*");
        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
        app.use(cors());
        next();
    });

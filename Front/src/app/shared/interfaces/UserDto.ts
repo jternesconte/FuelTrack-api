6ad5a4b19e032c4fcb8bfb1f05d5bf2dcac0a930
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-01-29 07:44:08 -0300

    feat: adds Dtos to login and register

--- Front/src/app/shared/interfaces/UserDto.ts
+++ Front/src/app/shared/interfaces/UserDto.ts
@@ -3,3 +3,15 @@ export interface UserDto {
    email: string,
    password: string
 }
+
+export interface userLoginDto {
+   email: string,
+   password: string
+}
+
+export interface UserRegisterDto {
+   name: string,
+   email: string,
+   password: string,
+   password2: string
+}

3741b80a6014e37e67ebb71118e08eb6f8fef335
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-03 12:57:57 -0300

    feat: adds fuel-register component

---
+++ Front/src/app/features/fuel-register/fuel-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fuel-register',
+  templateUrl: './fuel-register.component.html',
+  styleUrls: ['./fuel-register.component.css', '../../../styles.css']
+})
+export class FuelRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

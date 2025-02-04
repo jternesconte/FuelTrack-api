1356547bd03d137d8743c442a9ab98407d2c09e8
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-04 07:19:15 -0300

    feat: create the template for fuelRegister

--- Front/src/app/features/fuel-register/fuel-register.component.ts
+++ Front/src/app/features/fuel-register/fuel-register.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-fuel-register',
   templateUrl: './fuel-register.component.html',
-  styleUrls: ['./fuel-register.component.css', '../../../styles.css']
+  styleUrls: ['./fuel-register.component.css', '../../../styles.css'],
+  imports: [ReactiveFormsModule, FormsModule, CardModule, FloatLabelModule, ButtonModule]
 })
 export class FuelRegisterComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onFuelSubmit() {
+
   }
 
 }

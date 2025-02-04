b3b3277f979c0595d494eea2bfabfb16d0df548f
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-04 07:18:51 -0300

    feat: implements route for fuelCompoent

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CarProfileComponent } from './features/car-profile/car-profile.component';
 import { LoginPageComponent } from './features/login-page/login-page.component';
 import { UserCarsComponent } from './features/user-cars/user-cars.component';
+import { FuelRegisterComponent } from './features/fuel-register/fuel-register.component';
 
 export const routes: Routes = [
    {
@@ -15,5 +16,9 @@ export const routes: Routes = [
    {
       path: 'car/:carId',
       component: CarProfileComponent
+   },
+   {
+      path: 'fuel/:carId',
+      component: FuelRegisterComponent
    }
 ];

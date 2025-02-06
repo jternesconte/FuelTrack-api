6c058ee3c3d8ea092aed0f6b38d0525d284ae33b
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-06 07:51:51 -0300

    feat: adds a confirm dialog to logout button

--- Front/src/app/shared/components/app-header/app-header.component.ts
+++ Front/src/app/shared/components/app-header/app-header.component.ts
@@ -3,23 +3,58 @@ import { Toolbar } from 'primeng/toolbar';
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { Router } from '@angular/router';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html',
   styleUrls: ['./app-header.component.css'],
-  imports: [Toolbar, AvatarModule, ButtonModule]
+  imports: [Toolbar, AvatarModule, ButtonModule, ConfirmDialogModule, ToastModule],
+  providers: [ConfirmationService, MessageService]
 })
 export class AppHeaderComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit() {
   }
 
-  onLogout() {
+  onLogout(event: any) {
     localStorage.removeItem('token');
     this.router.navigate(['/login']);
+
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Are you sure that you want to logout?',
+      header: 'Confirmation',
+      closable: true,
+      closeOnEscape: true,
+      rejectButtonProps: {
+          label: 'Cancel',
+          severity: 'secondary',
+          outlined: true,
+      },
+      acceptButtonProps: {
+          label: 'Logout',
+      },
+      accept: () => {
+          this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'Successfully logout' });
+      },
+      reject: () => {
+          this.messageService.add({
+              severity: 'error',
+              summary: 'Rejected',
+              detail: 'You have rejected',
+              life: 3000,
+          });
+      },
+    });
   }
 
   onCars() {

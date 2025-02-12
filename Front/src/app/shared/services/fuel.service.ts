1cbc45af2d69aa500e30c5a26d5f1afd584ed852
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-12 07:29:49 -0300

    feat: adds fuelHisotry get route

--- Front/src/app/shared/services/fuel.service.ts
+++ Front/src/app/shared/services/fuel.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LastMonthsData } from '../../features/car-profile/car-profile.component';
+import { FuelHistory, LastMonthsData } from '../../features/car-profile/car-profile.component';
 import { FuelDto } from '../interfaces/FuelDto';
 
 @Injectable({
@@ -16,7 +16,11 @@ export class FuelService {
     return this.http.post(`http://localhost:3000/api/fuel/create/${carId}`, fuelInfo);
   }
 
-  getCarData(carId: number, months: number): Observable<LastMonthsData> {
+  getFuelData(carId: number, months: number): Observable<LastMonthsData> {
     return this.http.get<LastMonthsData>(`http://localhost:3000/api/fuel/average/${carId}/${months}`);
+  }
+
+  getFuelHistory(carId: number, months: number): Observable<FuelHistory[]> {
+    return this.http.get<FuelHistory[]>(`http://localhost:3000/api/fuel/history/${carId}/${months}`);
   }
 }

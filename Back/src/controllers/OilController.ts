efe04c1d290821e37b15e6c4f852694ccc63c010
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-20 12:44:49 -0300

    feat: adds oil controller

---
+++ Back/src/controllers/OilController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+export class OilController {
+
+   async newOilChange(req: Request, res: Response) {
+      try {
+
+      } catch {
+
+      }
+   }
+
+}

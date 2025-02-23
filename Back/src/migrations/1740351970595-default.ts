5cef91c67e68e38940539f4beea30afd04c308fb
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-23 20:08:19 -0300

    fix: removes wrong migrations

--- Back/src/migrations/1740351970595-default.ts
+++ Back/src/migrations/1740351970595-default.ts
@@ -1,16 +1,20 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Default1740171480345 implements MigrationInterface {
-    name = 'Default1740171480345'
+export class Default1740351970595 implements MigrationInterface {
+    name = 'Default1740351970595'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "ipva" DROP COLUMN "oil_type"`);
         await queryRunner.query(`ALTER TABLE "ipva" DROP COLUMN "car_km"`);
         await queryRunner.query(`ALTER TABLE "oil" ADD "car_km" numeric(12,1) NOT NULL DEFAULT '0'`);
+        await queryRunner.query(`ALTER TABLE "oil" ADD "oil_type" character varying(100) NOT NULL`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "oil" DROP COLUMN "oil_type"`);
         await queryRunner.query(`ALTER TABLE "oil" DROP COLUMN "car_km"`);
         await queryRunner.query(`ALTER TABLE "ipva" ADD "car_km" numeric(12,1) NOT NULL DEFAULT '0'`);
+        await queryRunner.query(`ALTER TABLE "ipva" ADD "oil_type" character varying(100) NOT NULL`);
     }
 
 }

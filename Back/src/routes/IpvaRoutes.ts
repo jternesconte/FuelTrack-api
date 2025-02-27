4cdf16ed2395ee295d71ce16c66c347e3b82466f
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-27 06:54:54 -0300

    feat: adds deleteIpva route

--- Back/src/routes/IpvaRoutes.ts
+++ Back/src/routes/IpvaRoutes.ts
@@ -7,3 +7,5 @@ export const ipvaRoutes = Router();
 ipvaRoutes.use(authenticateToken);
 
 ipvaRoutes.post('/paid/:carId', new IpvaController().newPaidIpva);
+
+ipvaRoutes.post('/deleteIpva/:ipvaId', new IpvaController().removeIpva);

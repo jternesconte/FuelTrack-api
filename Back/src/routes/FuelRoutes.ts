3fd6ec91237d3f0615138e7f8c4802803c237242
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-02-26 22:27:37 -0300

    feat: adds delete fuel route

--- Back/src/routes/FuelRoutes.ts
+++ Back/src/routes/FuelRoutes.ts
@@ -13,3 +13,5 @@ fuelRoutes.get('/average/:carId/:months', new FuelController().averageMonths);
 fuelRoutes.get('/history/:carId/:months', new FuelController().fuelHistory);
 
 fuelRoutes.post('/calculateFuel/:carId', new FuelController().calculateDistanceFuel);
+
+fuelRoutes.delete('/deleteFuel/:fuelId', new FuelController().deleteFuel);

7e3bd65b514a37068e15e29c814beac1fcefe96c
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-06 06:45:42 -0300

    feat: adds ipva-register components

---
+++ Front/src/app/features/ipva-register/ipva-register.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ipva-register',
+  templateUrl: './ipva-register.component.html',
+  styleUrls: ['./ipva-register.component.css']
+})
+export class IpvaRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

bf9e8839a18656710974a90bdbbb24d9b046100f
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2025-03-09 17:09:47 -0300

    feat: adds oil change html

---
+++ Front/src/app/features/oil-change/oil-change.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oil-change',
+  templateUrl: './oil-change.component.html',
+  styleUrls: ['./oil-change.component.css']
+})
+export class OilChangeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

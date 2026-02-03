07040d70355a6ef823370b9a58b484c34b592286
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2026-02-03 09:23:54 -0300

    feat: add userRoutes (RefreshToken and CheckAuth)

--- Back/src/routes/UserRoutes.ts
+++ Back/src/routes/UserRoutes.ts
@@ -9,3 +9,7 @@ userRoutes.post('/register', new UserController().newRegister);
 userRoutes.post('/login', new UserController().userLogin);
 
 userRoutes.put('/editUser', authenticateToken, new UserController().editUser);
+
+userRoutes.post('/refresh-token', new UserController().refreshToken);
+
+userRoutes.get('/check-auth', authenticateToken, new UserController().checkAuth);

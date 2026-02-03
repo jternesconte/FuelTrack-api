3c458c27f98fdae4bfb3d208216d2da526858397
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2026-02-03 09:23:19 -0300

    refactor: hanges ident and origins allowed

--- Back/src/index.ts
+++ Back/src/index.ts
@@ -8,31 +8,35 @@ import { oilRoutes } from './routes/OilRoutes';
 import { ipvaRoutes } from './routes/IpvaRoutes';
 
 AppDataSource.initialize().then(() => {
-   const app = express();
+    const app = express();
+    const cookieParser = require('cookie-parser');
 
-   app.use(express.json({ limit: '50mb' }));
-   app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+    app.use(express.json({ limit: '50mb' }));
+    app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+    app.use(cookieParser());
 
-   app.use(express.json());
+    app.use(express.json());
 
-   app.use(cors({
-    origin: '*',
-    methods: 'GET, PUT, POST, DELETE',
-    allowedHeaders: 'Content-Type, Authorization'
+    app.use(cors({
+        origin: 'http://localhost:4200',
+        credentials: true,
+        methods: 'GET, PUT, POST, DELETE',
+        allowedHeaders: 'Content-Type, Authorization'
     }));
 
-   app.use((req, res, next) => {
-       res.header("Access-Control-Allow-Origin", "*");
-       res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-       res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-       next();
-   });
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.header("Access-Control-Allow-Credentials", "true");
+        next();
+    });
 
-   app.use('/api/car', carRoutes);
-   app.use('/api/fuel', fuelRoutes);
-   app.use('/api/user', userRoutes);
-   app.use('/api/oil', oilRoutes);
-   app.use('/api/ipva', ipvaRoutes);
+    app.use('/api/car', carRoutes);
+    app.use('/api/fuel', fuelRoutes);
+    app.use('/api/user', userRoutes);
+    app.use('/api/oil', oilRoutes);
+    app.use('/api/ipva', ipvaRoutes);
 
-   return app.listen(process.env.PORT);
+    return app.listen(process.env.PORT);
 });

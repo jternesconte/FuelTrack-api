f8ba0c2233c8abc8b6255ae0c1b4632a3ec31402
João Pedro Ternes Conte <joaopedrotc04@gmail.com>
2026-02-10 14:24:57 -0300

    refactor: changes localstorage to cookies

--- Front/src/app/auth/auth.service.ts
+++ Front/src/app/auth/auth.service.ts
@@ -1,15 +1,33 @@
 import { inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpBackend, HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private handler: HttpBackend) {
+    this.http = new HttpClient(handler);
+  }
+
+  private http: HttpClient;
 
   isAuthenticated(): boolean {
-    return !!localStorage.getItem('token');
+    return !!document.cookie.includes('accessToken') || !!document.cookie.includes('refreshToken');
+  }
+
+  checkAuth(): Observable<boolean> {
+    return this.http.get<{ isAuthenticated: boolean }>('http://localhost:3000/api/user/check-auth', { withCredentials: true }).pipe(
+      map(res => res.isAuthenticated),
+      catchError(() => of(false))
+    );
+  }
+
+  refreshToken(): Observable<any> {
+    return this.http.post('http://localhost:3000/api/user/refresh-token', {}, { withCredentials: true });
   }
 
   redirectToLogin(): void {
